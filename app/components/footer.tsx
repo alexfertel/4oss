380165a45d58d1a089f280a27af3fd078bc4960f
Alexander Gonzalez <alexfertel97@gmail.com>
2025-04-20 20:48:32 +0200

    fix: proper footer anchor styles

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,23 +1,29 @@
 export default function Footer() {
   return (
     <footer className="flex items-center justify-center text-center h-10 text-zinc-600">
-      <a
-        className="inline-block hover:underline"
-        href="https://urlbox.com/screenshot-api"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <span className="text-zinc-900">Urlbox</span>
-      </a>
+      <p>
+        Powered by{" "}
+        <a
+          className="inline-block hover:underline text-zinc-900"
+          href="https://urlbox.com/screenshot-api"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Urlbox
+        </a>
+      </p>
       <span className="select-none">&thinsp;•&thinsp;</span>
-      <a
-        href="https://alexfertel.me"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="inline-block hover:underline transition-all duration-200"
-      >
-        Made by <span className="text-zinc-900">alexfertel</span>
-      </a>
+      <p>
+        Made by{" "}
+        <a
+          href="https://alexfertel.me"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block hover:underline transition-all duration-200 text-zinc-900"
+        >
+          alexfertel
+        </a>
+      </p>
     </footer>
   );
 }
